04f982512ad7762583dd89281257a0b0bc6d5af6
CH <chris@chrishouse.us>
2018-02-04 15:36:36 -0600

    (feat): #143 link repo issue opened by user to user detail component

--- src/apps/github/containers/repo-issues/issue-collection/issue-collection.component.ts
+++ src/apps/github/containers/repo-issues/issue-collection/issue-collection.component.ts
@@ -1,6 +1,9 @@
 import { Component, Input, ChangeDetectionStrategy } from '@angular/core';
-
+import { User } from '@app/github/shared/models';
 import { Issue } from '@app/github/shared/models';
+import { ActionsSubject, Store } from '@ngrx/store';
+import * as userActions from '@app/github/store/actions';
+import * as fromRoot from '@app/github/store/reducers';
 
 @Component({
   selector: 'app-issue-collection',
@@ -10,4 +13,14 @@ import { Issue } from '@app/github/shared/models';
 })
 export class IssueCollectionComponent {
   @Input() repoIssueCollection: Issue[];
+
+  constructor(
+    private store: Store<fromRoot.AppState>,
+    private actionsSubject: ActionsSubject){
+    
+  }
+
+  setUser(user: User) {
+    this.store.dispatch(new userActions.SetCurrentUserId(user.login));
+  }
 }

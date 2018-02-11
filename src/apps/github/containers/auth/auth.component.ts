6d6d04c78353719d807dd95b0f67fa43e3590e9d
CH <chris@chrishouse.us>
2018-02-11 17:54:14 -0600

    (feat): signed in successfully with github

--- src/apps/github/containers/auth/auth.component.ts
+++ src/apps/github/containers/auth/auth.component.ts
@@ -4,6 +4,8 @@ import { HttpErrorResponse, HttpClient } from '@angular/common/http';
 import { FormControl } from '@angular/forms';
 import { Observable } from 'rxjs/Rx';
 import { ActionsSubject, Store } from '@ngrx/store';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
 import * as userActions from '@app/github/store/actions';
 import * as fromRoot from '@app/github/store/reducers';
 import { GithubService } from '@app/github/shared/services';
@@ -16,27 +18,45 @@ import { User } from '@app/github/shared/models';
 export class UserAuthComponent implements OnInit {
 
     public code: any;
-    public token: string;
-
+    public token = '';
+    public user: any = null;
     constructor(
+        public afAuth: AngularFireAuth,
         private route: ActivatedRoute,
         private router: Router,
         private githubService: GithubService,
         private _http: HttpClient) {
+
+            firebase.auth().getRedirectResult().then((result) => {
+                if (result.credential) {
+                  // This gives you a GitHub Access Token. You can use it to access the GitHub API.
+                  this.token = result.credential.accessToken;
+                  // ...
+                }
+                // The signed-in user info.
+                this.user = result.user;
+              }).catch((error) => {
+                console.log(error);
+              });
     }
 
     ngOnInit() {
-        this.code = this.route.snapshot.queryParams['code'];
-        if (this.code) {
-            // redirect again to github to get baerer token
-            this.getToken();
-        }
     }
 
-    getToken() {
-        this.githubService.getApiToken(this.code).subscribe(data => {
-            this.token = data;
-        });
+    signIn() {
+        const provider = new firebase.auth.GithubAuthProvider();
+        firebase.auth().signInWithRedirect(provider);
+    }
+
+    signOut() {
+        firebase.auth().signOut().then(() => {
+            // Sign-out successful.
+            // todo: put this in the redux store
+            this.user = undefined;
+            this.token = '';
+          }).catch((error) => {
+            console.log(error);
+          });
     }
 
 }

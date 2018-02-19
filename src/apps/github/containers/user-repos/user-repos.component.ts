7fe0bedcb11b777e595f2bd1923914089f44b3b0
CH <chris@chrishouse.us>
2018-02-19 17:41:13 -0600

    (style): user-repo component

--- src/apps/github/containers/user-repos/user-repos.component.ts
+++ src/apps/github/containers/user-repos/user-repos.component.ts
@@ -1,17 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import { ActionsSubject, Store } from '@ngrx/store';
 import * as userActions from '@app/github/store/actions';
 import * as fromRoot from '@app/github/store/reducers';
 import { User, Repo } from '@app/github/shared/models';
+import { MatTableDataSource, MatSort } from '@angular/material';
 
 @Component({
     selector: 'app-user-repos',
     templateUrl: './user-repos.component.html',
     styleUrls: ['./user-repos.component.css']
 })
-export class UserReposComponent implements OnInit {
-
+export class UserReposComponent implements OnInit, AfterViewInit {
+    @ViewChild(MatSort) sort: MatSort;
     public repoCollection: Repo[];
+    dataSource = new MatTableDataSource(this.repoCollection);
+    displayedColumns = ['name', 'description', 'language',
+        'created_at', 'has_issues', 'forks_count', 'archived'];
 
     constructor(
         private store: Store<fromRoot.AppState>,
@@ -32,8 +36,16 @@ export class UserReposComponent implements OnInit {
         this.store.select(fromRoot.getUserRepo).subscribe(data => {
             if (data) {
                 this.repoCollection = data;
+                this.dataSource = new MatTableDataSource(this.repoCollection);
             }
         });
     }
 
+    /**
+     * Set the sort after the view init since this component will
+     * be able to query its view for the initialized sort.
+     */
+    ngAfterViewInit() {
+        this.dataSource.sort = this.sort;
+    }
 }

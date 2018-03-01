713e7742c76917ed6e2ecdbedef7f7fc833d8de2
CH <chris@chrishouse.us>
2018-02-28 19:09:00 -0600

    (feat): add photo to navbar when authenticated

--- src/apps/app.component.ts
+++ src/apps/app.component.ts
@@ -1,6 +1,7 @@
 import { MediaMatcher } from '@angular/cdk/layout';
 import { ChangeDetectorRef, Component } from '@angular/core';
-
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
 @Component({
   selector: 'app-house',
   templateUrl: './app.component.html',
@@ -8,11 +9,30 @@ import { ChangeDetectorRef, Component } from '@angular/core';
 })
 export class AppComponent {
   mobileQuery: MediaQueryList;
+  imageUrl = '';
   private _mobileQueryListener: () => void;
 
-  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+  constructor(changeDetectorRef: ChangeDetectorRef,
+    public afAuth: AngularFireAuth,
+    media: MediaMatcher) {
     this.mobileQuery = media.matchMedia('(max-width: 600px)');
     this._mobileQueryListener = () => changeDetectorRef.detectChanges();
     this.mobileQuery.addListener(this._mobileQueryListener);
+
+    this.loginCallback();
+  }
+
+  loginCallback() {
+    firebase.auth().getRedirectResult().then((result) => {
+      if (result.credential) {
+        // This gives you a GitHub Access Token. You can use it to access the GitHub API.
+      }
+      if (result.user !== null) {
+        // The signed-in user info.
+        this.imageUrl = result.user.photoURL;
+      }
+    }).catch((error) => {
+      console.log(error);
+    });
   }
 }

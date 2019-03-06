19dbb008f7f5727b33a49a888b31769ddc8ebe54
CH <chris@chrishouse.us>
2019-03-05 20:06:09 -0600

    (deps): ⬆️ angular to 7.2.7

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -1,18 +1,18 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/0.13/config/configuration-file.html
 
-module.exports = function (config) {
-  config.set({
+module.exports = function ( config ) {
+  config.set( {
     basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    frameworks: [ 'jasmine', '@angular-devkit/build-angular' ],
     plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require( 'karma-jasmine' ),
+      require( 'karma-chrome-launcher' ),
+      require( 'karma-jasmine-html-reporter' ),
+      require( 'karma-coverage-istanbul-reporter' ),
+      require( '@angular-devkit/build-angular/plugins/karma' )
     ],
-    client:{
+    client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
@@ -20,10 +20,10 @@ module.exports = function (config) {
       reports: [ 'html', 'lcovonly' ],
       fixWebpackSourcePaths: true
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: [ 'progress', 'kjhtml' ],
     port: 9876,
     browsers: [ 'Chrome' ],
     logLevel: config.LOG_INFO,
-    singleRun: false
-  });
+    singleRun: true
+  } );
 };

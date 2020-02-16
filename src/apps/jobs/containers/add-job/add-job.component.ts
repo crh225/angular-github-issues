8dbcf493b90ff2016e898f793cf88df4fb1579d2
CH <chris@chrishouse.us>
2020-02-16 17:50:58 -0600

    feature: deleting jobs from firestore

--- src/apps/jobs/containers/add-job/add-job.component.ts
+++ src/apps/jobs/containers/add-job/add-job.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { Jobs } from '../../shared';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-add-job',
@@ -8,13 +14,47 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 })
 export class AddJobComponent implements OnInit {
 
-  constructor(public snackBar: MatSnackBar) { }
+
+  _db: AngularFirestore;
+  jobs: Observable<any>;
+  jobForm = new FormGroup({
+    company: new FormControl('', Validators.required),
+    description: new FormControl('', Validators.required),
+    title: new FormControl('', Validators.required),
+    salary: new FormControl('')
+  });
+  jobCollectionRef: AngularFirestoreCollection<any>;
+
+  constructor(public snackBar: MatSnackBar, public afAuth: AngularFireAuth, db: AngularFirestore) {
+    this.jobs = db.collection('jobs').valueChanges();
+    this._db = db;
+    this.jobCollectionRef = db.collection<any>('jobs');
+    this.jobs = this.jobCollectionRef
+      .snapshotChanges()
+      .pipe(map(actions => {
+        return actions.map(a => {
+          const data = a.payload.doc.data();
+          const id = a.payload.doc.id;
+          return { id, ...data };
+        });
+      }));
+  }
 
   ngOnInit(): void {
   }
 
   addJob() {
-    this.snackBar.open('Adding Jobs is a work in progress.', 'Ok');
+    const shirtsCollection = this._db.collection<Jobs>('jobs');
+    shirtsCollection.add(
+      {
+        company: this.jobForm.get('company').value,
+        description: this.jobForm.get('description').value,
+        title: this.jobForm.get('title').value
+      }
+    );
+    this.snackBar.open('Job was added successfully', 'Ok', {
+      duration: 2000,
+    });
   }
 
 }
--- src/apps/jobs/shared/models/jobs.model.ts
+++ src/apps/jobs/shared/models/jobs.model.ts
@@ -2,6 +2,7 @@ export class Jobs {
     company: string;
     description: string;
     title: string;
+    id?: string;
     constructor(company: string = '', description: string = '', title: string = '') {
         this.company = company;
         this.description = description;
